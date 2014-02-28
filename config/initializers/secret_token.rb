3136923c072a85a861ced5e440ee3b0280087239
--get <alexreichert621@gmail.com>
2014-02-28 10:26:19 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoalStter::Application.config.secret_key_base = '464ebcfcbc397e454bcecb08f5a8aefc5420ddc18436d08bd718163b282c04011bb785e7753f172d7a83c9b4a8b347580fcf491e3ad27e246e65264e6e2d87cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoalStter::Application.config.session_store :cookie_store, key: '_goal_stter_session'
